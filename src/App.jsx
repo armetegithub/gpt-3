af21f8c7683f613bd0efdbce5395e6a341e01f51
Armando <44293310+armetegithub@users.noreply.github.com>
2022-12-29 00:17:48 +0100

    Aplicación gpt-3

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
+
+import { Navbar, CTA, Brand} from './components';
+
+import './App.css'
+import './index.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Blog />
+      <CTA />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
